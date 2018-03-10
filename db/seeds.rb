89ba0ba7c074f265d2f9309e281c92208eb75a34
lauraszyms <lauraszyms@gmail.com>
2018-03-10 01:46:02 -0500

    added seed data for suits and wands cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Suit.create([
+  {:name => "Major Arcana", :key_words => "abc"},
+  {:name => "Wands", :key_words => "Career, Creativity, Faith, Growth, Ideas, Innovation, Projects, Innovation, Risk, Energy, Action, Enthusiasm, Optimism, Desire, Passion, Perception, Determination, Will, Defiant, Defensive, Daring, Ecstatic, Pride, Ego, Restlessness."},
+  {:name => "Swords", :key_words => "Strife. Tension. Conflict. Struggle. Communication. Travel. Powerful. Intelligence. Foresight. Boldness. Reason. Logic. Sarcasm. Threats. Legal Actions. Surgery. Cutting. Accidents. Justice. Truth. Detachments. Separations. Moving On. Mental Activity."},
+  {:name => "Cups", :key_words => "Emotions. Moods. Affections. Artistic Abilities. Creativity. Nurturance. Love. Relationships. Empathy. Intuition. Sensitivity. Dreams. Fantasies. Comfort. Soothing. Flowing. Healing. Counseling. Psychic Abilities. Receptive. Yielding. Secretive. Merging. Fantasy."},
+  {:name => "Pentacles", :key_words => "Money. Material World. Property. Inheritance. Wealth. Business. Possessions. Savings. Talents. Training. Education. Study. Time. Giving and Receiving. Profit and Loss. Tangible Reality. Work. Values. Mother Nature. Emotional Security. Sensual Pleasure. Rewards."}
+])
+
+Card.create([
+  {:name => "Ace of Wands", :suit_id => 2, :key_words => "New Beginnings. Inspiration. Innovation. Virility."},
+  {:name => "Two of Wands", :suit_id => 2, :key_words => "Development. Achievement. Pride. Collaboration."},
+  {:name => "Three of Wands", :suit_id => 2, :key_words => "Visionary. Creativity. Initial Success. Enterprising."},
+  {:name => "Four of Wands", :suit_id => 2, :key_words => "Celebration. Blessings. Harmony. Bountiful Harvest."},
+  {:name => "Five of Wands", :suit_id => 2, :key_words => "Competition. Brainstorming. Male Bonding. Sport."},
+  {:name => "Six of Wands", :suit_id => 2, :key_words => "Triumph. Acclaim. Appreciation. Good News."},
+  {:name => "Seven of Wands", :suit_id => 2, :key_words => "Taking a Stand. Courage. Bravado. Self-Confidence"},
+  {:name => "Eight of Wands", :suit_id => 2, :key_words => "Excitement. Movement. Travel. Communication."},
+  {:name => "Nine of Wands", :suit_id => 2, :key_words => "Fortification. Self-Protection. Discipline. Determined."},
+  {:name => "Ten of Wands", :suit_id => 2, :key_words => "Perseverance. Patience. Home Stretch."},
+  {:name => "Page of Wands", :suit_id => 2, :key_words => "Daring. Optimistic. Exciting. Resourceful. Cheerful."},
+  {:name => "Knight of Wands", :suit_id => 2, :key_words => "Confidence. Vision. Spontaneous. Dynamic. Enthusiastic."},
+  {:name => "Queen of Wands", :suit_id => 2, :key_words => "Ambition. Popularity. Passion. Vivacity. Independence."},
+  {:name => "King of Wands", :suit_id => 2, :key_words => "Warmth. Generosity. Nobility. Strength. Leadership."}
+])
