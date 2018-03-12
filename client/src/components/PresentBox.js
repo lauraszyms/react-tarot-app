1c5b26b895cb6df66ccfabee63b4ddc4dd49f3eb
lauraszyms <lauraszyms@gmail.com>
2018-03-12 12:27:08 -0400

    added state cardLink to present box

--- client/src/components/PresentBox.js
+++ client/src/components/PresentBox.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 
 class PresentBox extends Component {
-	// handleClick = () => { this.props.onClick(this.props.idea.id) }
-  //
-	// handleDelete = () => { this.props.onDelete(this.props.idea.id) }
+  constructor() {
+    super();
+
+    this.state = {
+      cardLink: <img src="https://i.imgur.com/zGlHRnf.jpg" className="Present-box" alt="tarotBack" onClick={() => { alert("Show Card Info") }}/>
+    }
+  }
 
 	render () {
-	 return <img src="https://i.imgur.com/zGlHRnf.jpg" className="Present-box" alt="tarotBack" onClick={() => { alert("Show Card Info") }}/>
+	 return this.state.cardLink
   }
  }
 
