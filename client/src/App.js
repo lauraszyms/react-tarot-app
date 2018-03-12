943a0f5d53d6185caab0a2b5a1ddebcddd083a98
lauraszyms <lauraszyms@gmail.com>
2018-03-12 01:33:04 -0400

    added past/present/future card compnents

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TarotBack from './components/TarotBack'
+import PastBox from './components/PastBox'
+import PresentBox from './components/PresentBox'
+import FutureBox from './components/FutureBox'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <header className="App-header">
+          <TarotBack className="App-logo"/>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <PastBox/>
+        <PresentBox/>
+        <FutureBox/>
       </div>
     );
   }
