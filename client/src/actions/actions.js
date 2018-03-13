9f08acb2c524cf95d73470beff3992b6c24736f1
lauraszyms <lauraszyms@gmail.com>
2018-03-12 22:27:39 -0400

    added actions folder and file

---
+++ client/src/actions/actions.js
@@ -0,0 +1,23 @@
+export function addFutureCard(){
+  return {
+    type: "ADD_FUTURE_CARD_BOOK",
+    futureCardLink: futureCardLink: <img className="Future-box" src="https://i.imgur.com/bvdxin4.jpg"/>
+  }
+}
+
+
+export function addPresentCard(){
+  return {
+    type: "ADD_PRESENT_CARD",
+    presentCardLink: <img className="Present-box" src="https://i.imgur.com/bvdxin4.jpg"/>
+  }
+}
+
+
+
+export function addPastCard(){
+  return {
+    type: "ADD_PAST_CARD",
+    pastCardLink: <img className="Past-box" src="https://i.imgur.com/bvdxin4.jpg"/>
+  }
+}
