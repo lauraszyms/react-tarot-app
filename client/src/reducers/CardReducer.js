f438c6887fffbf8b8793f728b2bbb11e4c5d7635
lauraszyms <lauraszyms@gmail.com>
2018-03-12 20:37:57 -0400

    refactored cardReducer

--- client/src/reducers/CardReducer.js
+++ client/src/reducers/CardReducer.js
@@ -1,33 +1,12 @@
-let state;
+import React from 'react'
 
-function cardReducer(state = {cardLink: <img src="https://i.imgur.com/zGlHRnf.jpg" className="App-logo" alt="tarotBack"/>}, action){
+export default function cardReducer(state = {
+  cardLink: <img src="https://i.imgur.com/zGlHRnf.jpg" className="Future-box" alt="tarotBack"/>
+}, action) {
   switch (action.type) {
     case 'ADD_CARD':
-      return {cardLink: <img src="https://i.imgur.com/bvdxin4.jpg" className="App-logo" alt="tarotBack"/>}
-    case 'REMOVE_CARD':
-      return {cardLink: <img src="https://i.imgur.com/zGlHRnf.jpg" className="App-logo" alt="tarotBack"/>}
+      return {cardLink: <img src="https://i.imgur.com/bvdxin4.jpg" className="Future-box" alt="tarotBack"/>}
     default:
       return state;
   }
 }
-
-function dispatch(action){
-  state = changeCount(state, action);
-  render();
-};
-
-function render(){
-  let logo = document.getElementById('App-logo');
-  logo.textContent = state.cardLink;
-}
-
-dispatch({ type: '@@INIT' })
-let appLogo = document.getElementById('App-logo');
-
-appLogo.addEventListener('click', function() {
-    dispatch({ type: 'ADD_CARD' });
-})
-
-
-
-export default cardReducer;
