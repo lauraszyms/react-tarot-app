ac2d753f9f15012d0d5e9681368646b6aac5e929
lauraszyms <lauraszyms@gmail.com>
2018-03-15 01:07:55 -0400

    made combined reducer

--- client/src/reducers/CardReducer.js
+++ client/src/reducers/CardReducer.js
@@ -3,9 +3,15 @@ import React from 'react'
 export default function cardReducer(state = {
   futureCardLink: "https://i.imgur.com/zGlHRnf.jpg",
   presentCardLink: "https://i.imgur.com/zGlHRnf.jpg",
-  pastCardLink: "https://i.imgur.com/zGlHRnf.jpg"
+  pastCardLink: "https://i.imgur.com/zGlHRnf.jpg",
+  loading: false,
+  cards: []
 }, action) {
   switch (action.type) {
+    case 'LOADING_CARDS':
+      return Object.assign({}, state, {loading: true});
+    case 'FETCH_CARDS':
+      return { loading: false, cards: action.payload} ;
     case 'ADD_PRESENT_CARD':
       return {...state, presentCardLink: "https://i.imgur.com/bvdxin4.jpg"}
     case 'ADD_FUTURE_CARD':
