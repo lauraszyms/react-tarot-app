3d5b6e22c859dc48c24b98ff8081719d4856f84b
lauraszyms <lauraszyms@gmail.com>
2018-03-16 15:35:01 -0400

    removed from app.js

--- client/src/components/TarotBack.js
+++ client/src/components/TarotBack.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions/actions';
 
 export default class TarotBack extends React.Component {
+
   render() {
+
     return <img src="https://i.imgur.com/zGlHRnf.jpg" className="App-logo" alt="tarotBack" onClick={() => { alert("Shuffling!!") }}/>;
   }
  }
