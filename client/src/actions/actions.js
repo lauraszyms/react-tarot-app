8706028add4ea03baf972024c4c5b66bb8263b9b
lauraszyms <lauraszyms@gmail.com>
2018-03-16 02:28:22 -0400

    added actions to fetch cards and deal with errors

--- client/src/actions/actions.js
+++ client/src/actions/actions.js
@@ -1,17 +1,53 @@
 import React from 'react'
+import fetch from 'isomorphic-fetch';
+
+
+export function cardsHasErrored(bool) {
+    return {
+        type: 'CARDS_HAS_ERRORED',
+        hasErrored: bool
+    };
+}
+export function cardsIsLoading(bool) {
+    return {
+        type: 'CARDS_IS_LOADING',
+        isLoading: bool
+    };
+}
+export function cardsFetchDataSuccess(cards) {
+    return {
+        type: 'CARDS_FETCH_DATA_SUCCESS',
+        cards
+    };
+}
+
+export function cardsFetchData(url) {
+    return (dispatch) => {
+        dispatch(cardsIsLoading(true));
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw Error(response.statusText);
+                }
+                dispatch(cardsIsLoading(false));
+                return response;
+            })
+            .then((response) => response.json())
+            .then((cards) => dispatch(cardsFetchDataSuccess(cards)))
+            .catch(() => dispatch(cardsHasErrored(true)));
+    };
+}
 
 export function addFutureCard(){
   return {
-    type: "ADD_FUTURE_CARD",
-    futureCardLink: <img className="Future-box" src="https://i.imgur.com/bvdxin4.jpg"/>
+    type: "ADD_FUTURE_CARD"
   }
 }
 
 
 export function addPresentCard(){
   return {
-    type: "ADD_PRESENT_CARD",
-    presentCardLink: <img className="Present-box" src="https://i.imgur.com/bvdxin4.jpg"/>
+    type: "ADD_PRESENT_CARD"
   }
 }
 
@@ -19,7 +55,6 @@ export function addPresentCard(){
 
 export function addPastCard(){
   return {
-    type: "ADD_PAST_CARD",
-    pastCardLink: <img className="Past-box" src="https://i.imgur.com/bvdxin4.jpg"/>
+    type: "ADD_PAST_CARD"
   }
 }
