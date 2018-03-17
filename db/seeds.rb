ae47fc4caac3a4b965f7e476095ab68e63dcc2f6
lauraszyms <lauraszyms@gmail.com>
2018-03-17 01:38:35 -0400

    updated seed data imgs

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,18 @@ Suit.create([
 ])
 
 Card.create([
-  {:name => "Ace of Wands", :suit_id => 2, :key_words => "New Beginnings. Inspiration. Innovation. Virility.", :img => "https://i.imgur.com/13YSZAC.jpg"},
-  {:name => "Two of Wands", :suit_id => 2, :key_words => "Development. Achievement. Pride. Collaboration.", :img => "https://i.imgur.com/0IIzZF6.jpg"},
-  {:name => "Three of Wands", :suit_id => 2, :key_words => "Visionary. Creativity. Initial Success. Enterprising.", :img => "https://i.imgur.com/1GgqX6E.jpg"},
-  {:name => "Four of Wands", :suit_id => 2, :key_words => "Celebration. Blessings. Harmony. Bountiful Harvest.", :img => "https://i.imgur.com/UNIgFQ7.jpg"},
-  {:name => "Five of Wands", :suit_id => 2, :key_words => "Competition. Brainstorming. Male Bonding. Sport.", :img => "https://i.imgur.com/5hVE8Nf.jpg"},
-  {:name => "Six of Wands", :suit_id => 2, :key_words => "Triumph. Acclaim. Appreciation. Good News.", :img => "https://i.imgur.com/at8hW0l.jpg"},
-  {:name => "Seven of Wands", :suit_id => 2, :key_words => "Taking a Stand. Courage. Bravado. Self-Confidence", :img => "https://i.imgur.com/HBpmJg7.jpg"},
-  {:name => "Eight of Wands", :suit_id => 2, :key_words => "Excitement. Movement. Travel. Communication.", :img => "https://i.imgur.com/9I7r3VE.jpg"},
-  {:name => "Nine of Wands", :suit_id => 2, :key_words => "Fortification. Self-Protection. Discipline. Determined.", :img => "https://i.imgur.com/tu9RMrb.jpg"},
-  {:name => "Ten of Wands", :suit_id => 2, :key_words => "Perseverance. Patience. Home Stretch.", :img => "https://i.imgur.com/F2KZbAy.jpg"},
-  {:name => "Page of Wands", :suit_id => 2, :key_words => "Daring. Optimistic. Exciting. Resourceful. Cheerful.", :img => "https://i.imgur.com/52EPA1T.jpg"},
-  {:name => "Knight of Wands", :suit_id => 2, :key_words => "Confidence. Vision. Spontaneous. Dynamic. Enthusiastic.", :img => "https://i.imgur.com/fIKfU5Y.jpg"},
-  {:name => "Queen of Wands", :suit_id => 2, :key_words => "Ambition. Popularity. Passion. Vivacity. Independence.", :img => "https://i.imgur.com/hB9E2E4.jpg"},
-  {:name => "King of Wands", :suit_id => 2, :key_words => "Warmth. Generosity. Nobility. Strength. Leadership.", :img => "https://i.imgur.com/bvdxin4.jpg"}
+  {:name => "Ace of Wands", :suit_id => 2, :key_words => "New Beginnings. Inspiration. Innovation. Virility.", :img => "https://i.imgur.com/xKyhlGm.png"},
+  {:name => "Two of Wands", :suit_id => 2, :key_words => "Development. Achievement. Pride. Collaboration.", :img => "https://i.imgur.com/8ZMXFFb.png"},
+  {:name => "Three of Wands", :suit_id => 2, :key_words => "Visionary. Creativity. Initial Success. Enterprising.", :img => "https://i.imgur.com/yObcQq6.png"},
+  {:name => "Four of Wands", :suit_id => 2, :key_words => "Celebration. Blessings. Harmony. Bountiful Harvest.", :img => "https://i.imgur.com/VS5qI99.png"},
+  {:name => "Five of Wands", :suit_id => 2, :key_words => "Competition. Brainstorming. Male Bonding. Sport.", :img => "https://i.imgur.com/QEMnUm4.png"},
+  {:name => "Six of Wands", :suit_id => 2, :key_words => "Triumph. Acclaim. Appreciation. Good News.", :img => "https://i.imgur.com/ictubSx.png"},
+  {:name => "Seven of Wands", :suit_id => 2, :key_words => "Taking a Stand. Courage. Bravado. Self-Confidence", :img => "https://i.imgur.com/xpzdGNX.png"},
+  {:name => "Eight of Wands", :suit_id => 2, :key_words => "Excitement. Movement. Travel. Communication.", :img => "https://i.imgur.com/FGPcnRw.png"},
+  {:name => "Nine of Wands", :suit_id => 2, :key_words => "Fortification. Self-Protection. Discipline. Determined.", :img => "https://i.imgur.com/kKHw3H2.png"},
+  {:name => "Ten of Wands", :suit_id => 2, :key_words => "Perseverance. Patience. Home Stretch.", :img => "https://i.imgur.com/v21tAVR.png"},
+  {:name => "Page of Wands", :suit_id => 2, :key_words => "Daring. Optimistic. Exciting. Resourceful. Cheerful.", :img => "https://i.imgur.com/sPjihet.png"},
+  {:name => "Knight of Wands", :suit_id => 2, :key_words => "Confidence. Vision. Spontaneous. Dynamic. Enthusiastic.", :img => "https://i.imgur.com/Y3rTPq4.png"},
+  {:name => "Queen of Wands", :suit_id => 2, :key_words => "Ambition. Popularity. Passion. Vivacity. Independence.", :img => "https://i.imgur.com/hQkyu3Y.png"},
+  {:name => "King of Wands", :suit_id => 2, :key_words => "Warmth. Generosity. Nobility. Strength. Leadership.", :img => "https://i.imgur.com/3il1oLK.png"}
 ])
