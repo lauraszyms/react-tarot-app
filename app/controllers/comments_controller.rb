c83a5961fe8b6fcc42f80a0853cd1f926cb13746
lauraszyms <lauraszyms@gmail.com>
2018-03-17 23:51:30 -0400

    added new and create to commentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,27 @@
 class CommentsController < ApplicationController
 
   def index
-    @comments = Comment.all
-    render json: @comments
+   @comments = Comment.all
+   render json: @comments
   end
+
+  def new
+   @comment = Comment.new
+  end
+
+  def create
+   @comment = Comment.create(comment_params)
+    if @comment.valid?
+     @comment.save
+    end
+  end
+
+  private
+
+  def comment_params
+   params.require(:comment).permit(:name, :email, :content)
+  end
+
+
 
 end
