d396f52d1ca8c078284474bd21e236ad059840d3
lauraszyms <lauraszyms@gmail.com>
2018-04-02 08:10:04 -0400

    combined reducers

--- client/src/reducers/CardReducer.js
+++ client/src/reducers/CardReducer.js
@@ -1,33 +1,41 @@
-export function cardsHasErrored(state = false, action) {
-    switch (action.type) {
-        case 'CARDS_HAS_ERRORED':
-            return action.hasErrored;
-        default:
-            return state;
-    }
-}
-export function cardsIsLoading(state = false, action) {
-    switch (action.type) {
-        case 'CARDS_IS_LOADING':
-            return action.isLoading;
-        default:
-            return state;
-    }
-}
-export function cards(state = [], action) {
+// export function cardsHasErrored(state = false, action) {
+//     switch (action.type) {
+//         default:
+//             return state;
+//     }
+// }
+// export function cardsIsLoading(state = false, action) {
+//     switch (action.type) {
+//         default:
+//             return state;
+//     }
+// }
+export function cards(state = {index: [], isLoading: false, hasErrored: false}, action) {
     switch (action.type) {
         case 'CARDS_FETCH_DATA_SUCCESS':
-            return action.cards;
+            return {...state, index: action.cards };
+        case 'LIKE_SUCCESS':
+            return {...state, index: state.index.map((card) => card.id === action.card.id ? action.card : card)}
+            // const cardToReplace = state.find(function(card){ return card.id === action.card.id})
+            // const index = state.indexOf(cardToReplace)
+            // const newState = Object.assign([], state)
+            // newState[index] = action.card
+            // return newState
+        case 'CARDS_IS_LOADING':
+          return {...state, isLoading: action.isLoading};
+        case 'CARDS_HAS_ERRORED':
+          return {...state, hasErrored: action.hasErrored}
         default:
             return state;
     }
 }
 
-export function addLike(state = [], action) {
-    switch (action.type) {
-        case 'ADD_LIKE':
-            return action.addLike;
-        default:
-            return state;
-    }
-}
+// export function likes(state = [], action) {
+//     switch (action.type) {
+//         case 'ADD_LIKE':
+//         debugger
+//             return action.addLike;
+//         default:
+//             return state;
+//     }
+// }
